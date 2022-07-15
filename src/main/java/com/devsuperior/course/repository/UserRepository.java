60f57e6e17e2eb39d59fd3f628b080f1564565ac
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-07-14 23:21:36 -0300

    Service layer, component registration

--- src/main/java/com/devsuperior/course/repository/UserRepository.java
+++ src/main/java/com/devsuperior/course/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.devsuperior.course.repository;
 import com.devsuperior.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
